19514eb44a00748402a433ec961d73b5318554f8
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-11-10 19:07:16 +0100

    Se añaden las ultimas modificaciones al programa

--- 1. homeworks/1. Hexabot/HM.m
+++ 1. homeworks/1. Hexabot/HM.m
@@ -4,9 +4,9 @@ function HTM = HM(a, alpha, d, theta)
     rz = cat(2,rz,transpose([0 0 0 1]));
     tz = transl(0,0,d);
     
+    tx = transl(a,0,0);
     rx = [rotx(alpha); [0 0 0]];
     rx = cat(2,rx,transpose([0 0 0 1]));
-    tx = transl(a,0,0);
-   
-    HTM =  rz*tz*rx*tx;
+
+    HTM =  rz*tz*tx*rx;
 end
